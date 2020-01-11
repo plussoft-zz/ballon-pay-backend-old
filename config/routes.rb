3a81876f7a6052ea83a2d67266268761528943c4
Cleber Gomes <fclebinho@gmail.com>
2020-01-10 21:44:28 -0300

    Versionamento

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   use_doorkeeper
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1) do
+      resources :products
+    end
+
+    # scope module: :v2, constraints: ApiConstraints.new(version: 2, default: true) do
+    #   resources :products
+    # end
+  end
 end
